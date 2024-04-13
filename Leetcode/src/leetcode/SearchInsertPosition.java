9116fa37d5d9de9b48a5e9de477d65c032cb445c
21120589 <truonganhtuan3092@gmail.com>
2024-04-13 15:22:41 +0700

    add file

---
+++ Leetcode/src/leetcode/SearchInsertPosition.java
@@ -0,0 +1,23 @@
+package src.leetcode;
+
+public class SearchInsertPosition {
+	 public int searchInsert(int[] nums, int target) {
+	        int left = 0;
+	        int right = nums.length-1;
+	        int mid = (int)(left+right)/2;
+	        while(left<=right) {
+	        	if (target == nums[mid]) {
+	        		return mid;
+	        	}
+	        	if (target < nums[mid]) {
+	        		right = mid -1;
+	        		mid = (int)(left+right)/2;
+	        	}
+	        	else {
+	        		left = mid + 1;
+	        		mid = (int)(left+right)/2;
+	        	}
+	        }
+	        return left;
+	 }
+}
